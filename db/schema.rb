6613318825a25f2669d91a548d48779db88a3558
mozukraken <mozukraken@gmail.com>
2018-03-23 23:46:32 +0900

    インデックスの追加,テストの作成.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180319160028) do
+ActiveRecord::Schema.define(version: 20180323142220) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end

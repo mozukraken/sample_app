316ff05dfd9b59386a3b84d25d37b00a10e70790
mozukraken <mozukraken@gmail.com>
2018-03-24 22:08:03 +0900

    “カラム追加,テスト作成.”

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -54,10 +54,13 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
-  test "email addresses should be saves as lower-case" do
-    mixed_case_email = "Foo@ExaMple.COm"
-    @user.email = mixed_case_email
-    @user.save
-    assert_equal mixed_case_email.downcase, @user.reload.email
+  test "password should be present(nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 end

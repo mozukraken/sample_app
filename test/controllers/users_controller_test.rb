cf83df4f397c0aa8622a50bc2c33ed679f55f270
mozukraken <mozukraken@gmail.com>
2018-04-10 01:24:48 +0900

    管理者ユーザーがユーザーを削除出来るようにした

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -12,7 +12,6 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
 
-
   test "should get new" do
     get signup_path
     assert_response :success
